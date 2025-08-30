94406ecd3d9365c94fb79a4b25c295780d67eae3
UniQodex <theuniqodex@gmail.com>
2025-08-30 21:12:35 +0200

    example lua files

---
+++ example_lua_folder/example_1.lua
@@ -0,0 +1,8 @@
+-- @class Math
+-- @desc My Add function is very cool!
+-- @param x, number, Value 1
+-- @param y, number, Value 2
+-- @return Sum of two numbers
+function Add(x,y)
+    return x+y
+end
---
+++ example_lua_folder/subfolder/example_2.lua
@@ -0,0 +1,8 @@
+-- @class Math
+-- @desc My Remove function is very cool!
+-- @param x, number, Value 1
+-- @param y, number, Value 2
+-- @return Difference after subtracting
+function Remove(x,y)
+    return x-y
+end
